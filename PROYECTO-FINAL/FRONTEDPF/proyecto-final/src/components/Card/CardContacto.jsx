542eae7bcb4e9a63bc7a88a06d8b8997ffd021a2
Bego <suarez.mbego@gmail.com>
2023-03-06 18:39:11 +0100

    card

--- PROYECTO-FINAL/FRONTEDPF/proyecto-final/src/components/Card/CardContacto.jsx
+++ PROYECTO-FINAL/FRONTEDPF/proyecto-final/src/components/Card/CardContacto.jsx
@@ -4,8 +4,6 @@ export default function CardContacto({ datos }) {
   console.log(datos);
   return datos.map((datos) => (
     <>
-      {/* recibimos la datos y con ella ponemos una card pero no ponemos
-          datos sino que lo desestructuro en sus propiedades */}
       <div className="Wrap" key={datos.id} display="flex">
         <div className="imagen">
           <img
--- PROYECTO-FINAL/FRONTEDPF/proyecto-final/src/components/Card/CardMedicina.jsx
+++ PROYECTO-FINAL/FRONTEDPF/proyecto-final/src/components/Card/CardMedicina.jsx
@@ -1,27 +1,30 @@
-import "./CardMedicina.css";
+import "./CardContacto.css";
 
-export default function CardMedicina({ datos }) {
+export default function CardContacto({ datos }) {
   console.log(datos);
   return datos.map((datos) => (
     <>
+      {/* recibimos la datos y con ella ponemos una card pero no ponemos
+          datos sino que lo desestructuro en sus propiedades */}
       <div className="Wrap" key={datos.id} />
       <img
         className="imagen"
-        src={`http://localhost:3000/${datos.fotoMedicina}`}
+        src={`http://localhost:3000/${datos.fotoContacto}`}
         max-width="400px"
         margin="0"
-        alt={datos.nombreMedicina}
+        alt={datos.nombre}
       />
       {/*//van
           apareciendo las propiedades que tiene el objeto */}
       <br />
       <div className="titulo">
-        <h2>{datos.nombreMedicina}</h2>
+        <h2>{datos.nombre}</h2>
       </div>
-      <div className="prescripción">
-        <div className="tituloComentarios">Para qué es...</div>
+      <div className="comentarios">
+        <div className="tituloComentarios">Quién es...</div>
         <div className="comentario">
-          <h1>{datos.indicacion}</h1>
+          <h1>{datos.contactoComentario}</h1>
+          <h1>{datos.telefono}</h1>
         </div>
       </div>
     </>
